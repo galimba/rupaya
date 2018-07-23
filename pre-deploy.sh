ef4aafa86d5fa9178061ed8a3964b2c2c545d21b
Rupaya <dev@rupayacoin.org>
2018-07-23 21:37:50 +0200

    Update pre-deploy.sh

--- pre-deploy.sh
+++ pre-deploy.sh
@@ -3,12 +3,14 @@
 OUTDIR=$TRAVIS_BUILD_DIR/out/$TRAVIS_PULL_REQUEST/$TRAVIS_JOB_NUMBER-$HOST
 mkdir -p $OUTDIR/bin
 
+echo $TRAVIS_BUILD_DIR
+
 ARCHIVE_CMD="zip"
 
 if [[ $HOST = "i686-w64-mingw32" ]]; then
-  ARCHIVE_NAME="windows-x86.zip"
+  ARCHIVE_NAME="rupx-windows-32bit.zip"
 elif [[ $HOST = "x86_64-w64-mingw32" ]]; then
-    ARCHIVE_NAME="windows-x64.zip"
+    ARCHIVE_NAME="rupx-windows-64bit.zip"
 elif [[ $HOST = "arm-linux-gnueabihf" ]]; then
     ARCHIVE_NAME="arm-x86.tar.gz"
     ARCHIVE_CMD="tar -czf"
@@ -16,19 +18,24 @@ elif [[ $HOST = "aarch64-linux-gnu" ]]; then
     ARCHIVE_NAME="arm-x64.tar.gz"
     ARCHIVE_CMD="tar -czf"
 elif [[ $HOST = "x86_64-unknown-linux-gnu" ]]; then
-    ARCHIVE_NAME="linux-x64.tar.gz"
+    ARCHIVE_NAME="rupx-linux-x64.tar.gz"
     ARCHIVE_CMD="tar -czf"
 elif [[ $HOST = "x86_64-apple-darwin11" ]]; then
     ARCHIVE_NAME="osx-x64.zip"
 fi
 
-cp $TRAVIS_BUILD_DIR/src/qt/rupaya-qt $OUTDIR/bin/ || cp $TRAVIS_BUILD_DIR/src/qt/rupaya-qt.exe $OUTDIR/bin/ || echo "no QT Wallet"
-cp $TRAVIS_BUILD_DIR/src/rupayad $OUTDIR/bin/ || cp $TRAVIS_BUILD_DIR/src/rupayad.exe $OUTDIR/bin/
-cp $TRAVIS_BUILD_DIR/src/rupaya-cli $OUTDIR/bin/ || cp $TRAVIS_BUILD_DIR/src/rupaya-cli.exe $OUTDIR/bin/
-strip "$OUTDIR/bin"/* || echo "nothing to strip"
-ls -lah $OUTDIR/bin
+if [[ $HOST = "x86_64-apple-darwin11" ]]; then
+    find $TRAVIS_BUILD_DIR -type f | grep -i Rupaya-Core.dmg$ | xargs -i cp {} $OUTDIR/bin
+else
+    cp $TRAVIS_BUILD_DIR/src/qt/rupaya-qt $OUTDIR/bin/ || cp $TRAVIS_BUILD_DIR/src/qt/rupaya-qt.exe $OUTDIR/bin/ || echo "no QT Wallet"
+    cp $TRAVIS_BUILD_DIR/src/rupayad $OUTDIR/bin/ || cp $TRAVIS_BUILD_DIR/src/rupayad.exe $OUTDIR/bin/
+    cp $TRAVIS_BUILD_DIR/src/rupaya-cli $OUTDIR/bin/ || cp $TRAVIS_BUILD_DIR/src/rupaya-cli.exe $OUTDIR/bin/
+    strip "$OUTDIR/bin"/* || echo "nothing to strip"
+    ls -lah $OUTDIR/bin
+fi
 
 cd $OUTDIR/bin
+ls -ah
 ARCHIVE_CMD="$ARCHIVE_CMD $ARCHIVE_NAME *"
 eval $ARCHIVE_CMD
 
